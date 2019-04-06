c3d173d75e2f9578d86dc6337ffddb6acf499e46
Senpaizuri <mjsleebos@gmail.com>
2019-04-06 15:26:11 +0200

    Init tooling

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const 
+    gulp = require("gulp"),
+    concat = require("gulp-concat"),
+    cssNano= require("gulp-cssnano"),
+    uglify = require("gulp-uglify"),
+    sass = require("gulp-sass"),
+    babel = require("gulp-babel")
+
+sass.compiler = require("node-sass")
+
+gulp.task("sass",()=>{
+    return gulp.src("./dev/scss/base.scss")
+        .pipe(sass({sourceMap:true}).on("error",sass.logError))
+        .pipe(gulp.dest("./src/css/"))
+})
+
+gulp.task("minify",async()=>{
+    return gulp.src([
+        "./src/css/base.css"
+    ])
+        .pipe(concat("index.css"))
+        .pipe(cssNano())
+        .pipe(gulp.dest("./src/opt/"))
+})
+
+gulp.task("babelify",()=>{
+    return gulp.src("./dev/js/app.js")
+        .pipe(babel())
+        .pipe(gulp.dest("./src/js/"))
+})
+
+gulp.task("uglify",()=>{
+    return gulp.src("./src/js/app.js")
+    .pipe(concat("app.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("./src/opt/"))
+})
+
+gulp.task("build",async()=>{
+    gulp.parallel("sass")
+})
+
+gulp.task("watch",()=>{
+    gulp.watch("dev/scss/*.scss",
+        gulp.series("sass","minify")
+    )
+    gulp.watch("dev/js/*.js",
+        gulp.series("babelify","uglify")
+    )
+})
